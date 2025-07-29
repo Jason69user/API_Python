6889ab584a58da015e20d79b664292c994617064
Dimitry <dartahilles@mail.ru>
2025-07-30 00:00:05 +0400

    Get запрос

---
+++ Joke.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://official-joke-api.appspot.com/jokes/random" # получаем ссылку на ресурс
+print(url)
+result = requests.get(url) # отправляем запрос GET
+print("Статус код : " + str(result.status_code))
+assert 200 == result.status_code # сверяем статус код с ожидаемым
+if result.status_code == 200:
+    print("Успешно, статус код верный")
+else:
+    print("Провал, статус код не верный")
+result.encoding = "utf-8" # используем стандартную кодировку
+print(result.text) # выводим текст
