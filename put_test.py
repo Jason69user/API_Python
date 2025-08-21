81edf4e29152e8681838ce1821d797b293e18c4d
Dimitry <dartahilles@mail.ru>
2025-08-21 13:03:21 +0400

    Работаем с PUT класса create_new_location

--- put_test.py
+++ put_test.py
@@ -1,7 +1,9 @@
 from create_new_location import create_new_location # импортируем клас
 
+new_address = 'Sanguinius street, RU'
+
 # создаем экземпляр класса
 location_manager = create_new_location()
+
 # вносим изменения в адрес
-new_address = 'Sanguinius street, RU'
 location_manager.put_request('06a2e4af4292b7467b9efaa99635a143', new_address)
